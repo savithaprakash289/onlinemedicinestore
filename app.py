65edb3ce18fc728aa51dcefd3f9162209c75dfd0
savithaprakash289 <136881941+savithaprakash289@users.noreply.github.com>
2023-10-17 17:24:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "admin@123"
+app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "online_medicines_store"
 
 mysql = MySQL(app)
